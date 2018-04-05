763d644fef82605b0fd69a328f51ce350bf580c7
The Heart of Raven <lord.arockalypse@gmail.com>
2018-04-05 20:40:19 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1089,8 +1089,8 @@ int64 static GetBlockValue(int nHeight, int64 nFees)
 {
     int64 nSubsidy = 50 * COIN;
 
-    // Subsidy is cut in half every 840000 blocks, which will occur approximately every 4 years
-    nSubsidy >>= (nHeight / 840000); // Liberalcoin: 840k blocks in ~4 years
+    // Subsidy is cut in half every 300000 blocks, which will occur approximately every 4 years
+    nSubsidy >>= (nHeight / 300000); // Liberalcoin: 300k blocks in ~4 years
 
     return nSubsidy + nFees;
 }
